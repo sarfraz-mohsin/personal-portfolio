4047e4073dcc382c589174bab33c2784a53b0e29
Sarfraz Mohsin <sarfrazmohsin17@gmail.com>
2022-11-11 19:04:21 +0530

    initial commit

---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://twitter.com/SarfrazMohsin2" className="home__social-icon" target="_blank">
+        <i class="uil uil-twitter-alt"></i>
+     </a>  
+      
+      <a href="https://www.linkedin.com/in/sarfraz-mohsin-301bb41a0/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+      </a>  
+      
+      <a href="https://github.com/sarfraz-mohsin" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+      </a>  
+    </div>
+  )
+}
+
+export default Social
